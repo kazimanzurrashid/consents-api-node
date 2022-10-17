d3114431c5c3f80326f86d4e9181773e4dfc92f3
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2022-10-18 01:31:51 +0600

    refactor: typo

--- src/routes/health-router.test.ts
+++ src/routes/health-router.test.ts
@@ -1,5 +1,5 @@
-import type EventsController from '../controllers/health-controller';
-import eventsRouter from './health-router';
+import type HealthController from '../controllers/health-controller';
+import healthRouter from './health-router';
 
 describe('healthRouter', () => {
   describe('GET /', () => {
@@ -8,9 +8,9 @@ describe('healthRouter', () => {
 
     beforeAll(() => {
       mockedControllerStatus = jest.fn(async () => Promise.resolve());
-      const router = eventsRouter({
+      const router = healthRouter({
         status: mockedControllerStatus
-      } as unknown as EventsController);
+      } as unknown as HealthController);
 
       match = router.stack.find(
         (x) => x.route.path === '/' && x.route.methods.get
